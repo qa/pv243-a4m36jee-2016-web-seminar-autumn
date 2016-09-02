49de4352c602c58951961bf22bf7c1a6aadc1109
Tomas Remes <tremes@redhat.com>
2016-09-02 12:26:45 +0200

    Initial commit.

---
+++ src/main/webapp/resources/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* App Module */
+
+var simpleChat = angular.module('simpleChat', [
+    'ngRoute'
+]);
+
+
+simpleChat.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.
+                when('/', {
+                    templateUrl: 'templates/main.html'
+                });
+    }
+]);
